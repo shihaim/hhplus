24d34dbb58f7ae1310a0f24becd314ce40c9d683
shihaim518 <hashi00518@gmail.com>
2024-03-22 18:03:26 +0900

    [멘토링] 객체에 의존하지 않고 Mocking하여 테스트 코드 작성

--- step1-1/hhplus-tdd-jvm/src/main/kotlin/io/hhplus/tdd/point/PointService.java
+++ step1-1/hhplus-tdd-jvm/src/main/kotlin/io/hhplus/tdd/point/PointService.java
@@ -22,10 +22,12 @@ public class PointService {
     }
 
     public UserPoint point(final Long userId) {
+        logger.info("call point method");
         return userPointTable.selectById(userId);
     }
 
     public UserPoint charge(final Long userId, final Long point) {
+        logger.info("call charge method");
         if (point < 0) {
             throw new IllegalArgumentException("충전할 포인트가 음수일 수 없습니다.");
         }
@@ -33,12 +35,15 @@ public class PointService {
         final UserPoint userPoint = userPointTable.selectById(userId);
         final Long chargePoint = userPoint.getPoint() + point;
 
-        pointHistoryTable.insert(userId, point, TransactionType.CHARGE, System.currentTimeMillis());
+        UserPoint result = userPointTable.insertOrUpdate(userId, chargePoint);
 
-        return userPointTable.insertOrUpdate(userId, chargePoint);
+        pointHistoryTable.insert(userId, point, TransactionType.CHARGE, result.getUpdateMillis());
+
+        return result;
     }
 
     public UserPoint use(final Long userId, final Long usePoint) {
+        logger.info("call use method");
         if (usePoint < 0) {
             throw new IllegalArgumentException("사용할 포인트가 음수일 수 없습니다.");
         }
@@ -50,12 +55,15 @@ public class PointService {
 
         final Long restPoint = findUserPoint.getPoint() - usePoint;
 
-        pointHistoryTable.insert(userId, usePoint, TransactionType.USE, System.currentTimeMillis());
+        UserPoint result = userPointTable.insertOrUpdate(userId, restPoint);
 
-        return userPointTable.insertOrUpdate(userId, restPoint);
+        pointHistoryTable.insert(result.getId(), usePoint, TransactionType.USE, result.getUpdateMillis());
+
+        return result;
     }
 
     public List<PointHistory> history(final Long userId) {
+        logger.info("call history method");
         return pointHistoryTable.selectAllByUserId(userId);
     }
 }
--- step1-1/hhplus-tdd-jvm/src/test/kotlin/io/hhplus/tdd/point/PointServiceTest.java
+++ step1-1/hhplus-tdd-jvm/src/test/kotlin/io/hhplus/tdd/point/PointServiceTest.java
@@ -2,40 +2,41 @@ package io.hhplus.tdd.point;
 
 import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.ThrowableAssert.ThrowingCallable;
+import static org.mockito.Mockito.doReturn;
 
+@ExtendWith(MockitoExtension.class)
 class PointServiceTest {
 
+    @InjectMocks
     PointService pointService;
+
+    @Mock
     UserPointTable userPointTable;
+
+    @Mock
     PointHistoryTable pointHistoryTable;
-
-    Long userId = 1L;
-    Long point = 10000L;
-
-    /**
-     * 테스트 코드 내 자주 사용하는 setUp용 @BeforEach를 통하여 진행
-     */
-    @BeforeEach
-    void setUp() {
-        userPointTable = new UserPointTable();
-        pointHistoryTable = new PointHistoryTable();
-        pointService = new PointService(userPointTable, pointHistoryTable);
-        userPointTable.insertOrUpdate(userId, point);
-    }
 
     /**
      * UserPointTable에서 조회시 null인 경우 새로운 객체를 생성하므로 id 조회 실패에 대한 실패 테스트는 작성 X
      */
     @Test
-    void 포인트조회성공() throws Exception {
+    void 포인트조회성공() {
         //given
+        Long userId = 1L;
+        Long point = 10000L;
+        UserPoint userPoint = new UserPoint(userId, point, System.currentTimeMillis());
+        doReturn(userPoint).when(userPointTable).selectById(userId);
 
         //when
         UserPoint findUserPoint = pointService.point(userId);
@@ -44,52 +45,94 @@ class PointServiceTest {
         assertThat(findUserPoint).isNotNull();
         assertThat(findUserPoint.getId()).isEqualTo(userId);
         assertThat(findUserPoint.getPoint()).isEqualTo(point);
-        assertThat(findUserPoint.getUpdateMillis()).isNotNull();
     }
 
     /**
      * 충전할 포인트가 음수이면 안되므로 실패 테스트 작성
      */
     @Test
-    void 포인트충전실패_충전할_포인트가_음수() throws Exception {
+    void 포인트충전실패_충전할_포인트가_음수() {
         //given
+        Long userId = 1L;
         Long chargePoint = -1L;
 
         //when
+        ThrowingCallable throwingCallable = () -> pointService.charge(userId, chargePoint);
 
         //then
-        assertThatThrownBy(() -> pointService.charge(userId, chargePoint))
+        assertThatThrownBy(throwingCallable)
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("충전할 포인트가 음수일 수 없습니다.");
+                .hasMessage("충전할 포인트가 음수일 수 없습니다.");
     }
 
     /**
      * 포인트 충전 성공 테스트 작성
      */
     @Test
-    void 포인트충전성공() throws Exception {
+    void 포인트충전성공() {
         //given
+        Long userId = 1L;
+        Long point = 10000L;
+        long updateMillis = System.currentTimeMillis();
+        UserPoint userPoint = new UserPoint(userId, point, updateMillis);
+        doReturn(userPoint).when(userPointTable).selectById(userId);
+
         Long chargePoint = 5000L;
+        UserPoint chargingPoint = new UserPoint(userId, userPoint.getPoint() + chargePoint, updateMillis);
+        doReturn(chargingPoint).when(userPointTable).insertOrUpdate(userId, userPoint.getPoint() + chargePoint);
 
         //when
-        Long totalPoint = pointService.charge(userId, chargePoint).getPoint();
+        UserPoint result = pointService.charge(userId, chargePoint);
 
         //then
-        assertThat(totalPoint).isEqualTo(point + chargePoint);
+        assertThat(result.getId()).isEqualTo(userId);
+        assertThat(result.getPoint()).isEqualTo(chargingPoint.getPoint());
+        assertThat(result.getUpdateMillis()).isEqualTo(updateMillis);
+    }
+
+    /**
+     * 포인트 충전 성공 이후 포인트 내역 저장에 대한 테스트 작성
+     *  + 포인트 사용 내역인지에 대한 테스트는 진행하지 않았음. 비즈니스 로직상 당연히 불가능한 부분이라고 생각
+     *  Q1. 물론 서비스 로직에서 충전을 하면 그 내역을 저장하기 마련인데, Mocking 테스트시 해당 내역의 대한 상태 검증을 해야하는 부분인가?
+     *      만약 상태 검증을 해야한다면 gwt 단계에서 pointHistory 이후에는 UserPoint를 반환하기에 어떻게 검증을 할 수 있는가?
+     */
+    @Test
+    void 포인트충전성공_포인트충전내역_저장성공() {
+        //given
+        Long userId = 1L;
+        Long point = 10000L;
+        long updateMillis = System.currentTimeMillis();
+        UserPoint userPoint = new UserPoint(userId, point, updateMillis);
+        doReturn(userPoint).when(userPointTable).selectById(userId);
+
+        Long chargePoint = 5000L;
+        UserPoint chargingUserPoint = new UserPoint(userId, point + chargePoint, updateMillis);
+        doReturn(chargingUserPoint).when(userPointTable).insertOrUpdate(userId, point + chargePoint);
+
+        TransactionType type = TransactionType.CHARGE;
+        PointHistory pointHistory = new PointHistory(-1L, userId, type, chargePoint, chargingUserPoint.getUpdateMillis());
+        doReturn(pointHistory).when(pointHistoryTable).insert(userId, chargePoint, type, chargingUserPoint.getUpdateMillis());
+
+        //when
+        pointService.charge(userId, chargePoint);
+
+        //then
     }
 
     /**
      * 사용할 포인트의 값이 음수이면 안되므로 실패 테스트 작성
      */
     @Test
-    void 포인트사용실패_사용할포인트가_음수() throws Exception {
+    void 포인트사용실패_사용할포인트가_음수() {
         //given
+        Long userId = 1L;
         Long usePoint = -1L;
 
         //when
+        ThrowingCallable throwingCallable = () -> pointService.use(userId, usePoint);
 
         //then
-        assertThatThrownBy(() -> pointService.use(userId, usePoint))
+        assertThatThrownBy(throwingCallable)
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("사용할 포인트가 음수일 수 없습니다.");
     }
@@ -98,40 +141,91 @@ class PointServiceTest {
      * 현재 가지고 있는 포인트보다 사용할 포인트의 값 더 크면 안되므로 실패 테스트 작성
      */
     @Test
-    void 포인트사용실패_사용할포인트가_가지고있는_포인트보다_큼() throws Exception {
+    void 포인트사용실패_사용할포인트가_가지고있는_포인트보다_큼() {
         //given
+        Long userId = 1L;
+        Long point = 10000L;
+        long updateMillis = System.currentTimeMillis();
+        UserPoint userPoint = new UserPoint(userId, point, updateMillis);
+        doReturn(userPoint).when(userPointTable).selectById(userId);
 
         //when
         Long usePoint = 10001L;
+        ThrowingCallable throwingCallable = () -> pointService.use(userId, usePoint);
 
         //then
-        assertThatThrownBy(() -> pointService.use(userId, usePoint))
+        assertThatThrownBy(throwingCallable)
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessageContaining("존재하는 포인트보다 클 수 없습니다.");
+    }
+
+    /**
+     * 포인트 이용 성공 이후 포인트 내역 저장에 대한 테스트 작성
+     *  + 포인트 충전 내역인지에 대한 테스트는 진행하지 않았음. 비즈니스 로직상 당연히 불가능한 부분이라고 생각
+     *
+     *  Q1. 위와 동일
+     */
+    @Test
+    void 포인트사용성공_포인트이용내역_저장성공() {
+        //given
+        Long userId = 1L;
+        Long point = 10000L;
+        long updateMillis = System.currentTimeMillis();
+        UserPoint userPoint = new UserPoint(userId, point, updateMillis);
+        doReturn(userPoint).when(userPointTable).selectById(userId);
+
+        Long usePoint = 10000L;
+        UserPoint usingUserPoint = new UserPoint(userId, userPoint.getPoint() - usePoint, updateMillis);
+        doReturn(usingUserPoint).when(userPointTable).insertOrUpdate(userId, userPoint.getPoint() - usePoint);
+
+        TransactionType type = TransactionType.USE;
+        PointHistory pointHistory = new PointHistory(-1L, userId, type, usePoint, updateMillis);
+        doReturn(pointHistory).when(pointHistoryTable).insert(userId, usePoint, type, updateMillis);
+
+        //when
+        pointService.use(userId, usePoint);
+
+        //then
     }
 
     /**
      * 포인트 사용 성공 테스트 작성
      */
     @Test
-    void 포인트사용성공() throws Exception {
+    void 포인트사용성공() {
         //given
+        Long userId = 1L;
+        Long point = 10000L;
+        long updateMillis = System.currentTimeMillis();
+        UserPoint userPoint = new UserPoint(userId, point, updateMillis);
+        doReturn(userPoint).when(userPointTable).selectById(userId);
+
+
+        Long usePoint = 10000L;
+        UserPoint usingUserPoint = new UserPoint(userId, userPoint.getPoint() - usePoint, updateMillis);
+        doReturn(usingUserPoint).when(userPointTable).insertOrUpdate(userId, userPoint.getPoint() - usePoint);
+
+        TransactionType type = TransactionType.USE;
+        PointHistory pointHistory = new PointHistory(-1L, userId, type, usePoint, updateMillis);
+        doReturn(pointHistory).when(pointHistoryTable).insert(userId, usePoint, type, updateMillis);
 
         //when
-        Long usePoint = 10000L;
-        Long restPoint = pointService.use(userId, usePoint).getPoint();
+        UserPoint result = pointService.use(userId, usePoint);
 
         //then
-        assertThat(restPoint).isEqualTo(point - usePoint);
-
+        assertThat(result.getId()).isEqualTo(usingUserPoint.getId());
+        assertThat(result.getPoint()).isEqualTo(usingUserPoint.getPoint());
+        assertThat(result.getUpdateMillis()).isEqualTo(usingUserPoint.getUpdateMillis());
     }
 
     /**
      * 포인트 내역 조회시 충전/사용한 내역이 없는 빈 List 테스트 작성
      */
     @Test
-    void 포인트내역조회성공_충전및사용_내역이_없음() throws Exception {
+    void 포인트내역조회성공_충전및사용_내역이_없음() {
         //given
+        Long userId = 1L;
+        doReturn(List.of()).when(pointHistoryTable).selectAllByUserId(userId);
 
         //when
         List<PointHistory> findPointHistory = pointService.history(userId);
@@ -141,57 +235,21 @@ class PointServiceTest {
     }
 
     /**
-     * 포인트 내역 조회시 충전한 내역의 테스트 작성
-     */
-    @Test
-    void 포인트내역조회성공_충전_내역() throws Exception {
-        //given
-        Long chargePoint = 5000L;
-        pointService.charge(userId, chargePoint);
-
-        //when
-        List<PointHistory> findPointHistory = pointService.history(userId);
-
-        //then
-        assertThat(findPointHistory.get(0).getUserId()).isEqualTo(userId);
-        assertThat(findPointHistory.get(0).getAmount()).isEqualTo(chargePoint);
-        assertThat(findPointHistory.get(0).getType()).isEqualTo(TransactionType.CHARGE);
-    }
-
-    /**
-     * 포인트 내역 조회시 사용한 내역의 테스트 작성
-     */
-    @Test
-    void 포인트내역조회성공_사용_내역() throws Exception {
-        //given
-        Long usePoint = 5000L;
-        pointService.use(userId, usePoint);
-
-        //when
-        List<PointHistory> findPointHistory = pointService.history(userId);
-
-        //then
-        assertThat(findPointHistory.get(0).getUserId()).isEqualTo(userId);
-        assertThat(findPointHistory.get(0).getAmount()).isEqualTo(usePoint);
-        assertThat(findPointHistory.get(0).getType()).isEqualTo(TransactionType.USE);
-    }
-
-    /**
      * 포인트 내역 조회시 다건 내역의 테스트 작성
      */
     @Test
-    void 포인트내역조회성공_다건_내역() throws Exception {
+    void 포인트내역조회성공_다건_내역() {
         //given
-        Long chargePoint = 5000L;
-        pointService.charge(userId, chargePoint);
-
-        Long usePoint = 5000L;
-        pointService.use(userId, usePoint);
+        Long userId = 1L;
+        doReturn(List.of(
+                new PointHistory(1L, 1L, TransactionType.CHARGE, 5000L, System.currentTimeMillis()),
+                new PointHistory(2L, 1L, TransactionType.USE, 5000L, System.currentTimeMillis())
+        )).when(pointHistoryTable).selectAllByUserId(userId);
 
         //when
-        List<PointHistory> findPointHistory = pointService.history(userId);
+        List<PointHistory> result = pointService.history(userId);
 
         //then
-        assertThat(findPointHistory.size()).isEqualTo(2);
+        assertThat(result.size()).isEqualTo(2);
     }
 }
