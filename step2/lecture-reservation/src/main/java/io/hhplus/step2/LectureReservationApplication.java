ead2afff894b0b33a6de91db3bd17f426ec1beab
shihaim <hashi00518@gmail.com>
2024-03-23 23:25:19 +0900

    step2 first commit

---
+++ step2/lecture-reservation/src/main/java/io/hhplus/step2/LectureReservationApplication.java
@@ -0,0 +1,14 @@
+package io.hhplus.step2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
+
+@SpringBootApplication
+public class LectureReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LectureReservationApplication.class, args);
+    }
+
+}
---
+++ step2/lecture-reservation/src/test/java/io/hhplus/step2/LectureReservationApplicationTests.java
@@ -0,0 +1,15 @@
+package io.hhplus.step2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+
+@Profile("test")
+@SpringBootTest
+class LectureReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
