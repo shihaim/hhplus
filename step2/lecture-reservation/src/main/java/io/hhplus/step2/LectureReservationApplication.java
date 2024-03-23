882367fe36b9950b893c56ada7ddbc9f209173da
shihaim <hashi00518@gmail.com>
2024-03-23 23:41:12 +0900

    step2 update commit

--- step2/lecture-reservation/src/main/java/io/hhplus/step2/LectureReservationApplication.java
+++ step2/lecture-reservation/src/main/java/io/hhplus/step2/LectureReservationApplication.java
@@ -2,7 +2,6 @@ package io.hhplus.step2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class LectureReservationApplication {
